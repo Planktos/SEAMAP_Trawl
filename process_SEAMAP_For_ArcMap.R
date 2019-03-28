9193e8dc3c68b32a1102cfb7ceb9a689c855db15
plantkos <kellylynnrobinson@gmail.com>
2019-03-28 14:27:13 -0500

    initial commit

---
+++ process_SEAMAP_For_ArcMap.R
@@ -0,0 +1,26 @@
+
+library(plyr)
+library(dplyr)
+library(stringr)
+library(data.table)
+library(xlsx)
+
+d <- as.data.frame(readxl::read_excel("FishBiomass_step3_Jellyfish.xlsx",col_names = T))
+
+d$AggGrp_20130430 <- ifelse(test = is.na(d$AggGrp_20130430), "large_jellyfish", d$AggGrp_20130430)
+
+d <- d[,c("AggGrp_20130430","Year","Season", "Month", "Day","STATIONID", "use_PopulationDensity_(No/m2)","use_BiomassDensity_(kg/m2)","use_Depth_(m)", "DECSLAT","DECSLON","DECELAT","DECELON"),]
+
+d <- plyr::rename(d, c("use_BiomassDensity_(kg/m2)" = "biomass_den_kg.m2"))
+d <- plyr::rename(d, c("use_PopulationDensity_(No/m2)" = "pop_den_no.m2"))
+d <- plyr::rename(d, c("use_Depth_(m)" = "depth_m"))
+
+d$biomass_den_kg.m3 <- d$biomass_den_kg.m2*d$depth_m
+d$pop_den_no.m3 <- d$pop_den_no.m2*d$depth_m
+
+d <- d %>% rowwise() %>% mutate(DECLAT_ctr = mean(DECSLAT, DECELAT, na.rm = T), DECLON_ctr = mean(DECSLON, DECELON, na.rm = T))
+
+#subset data for ArcMAp
+a <- d[,c(1:6,9,14:17)]
+
+write.table(a, file = "jellyfish_test_arc.txt", sep =  "\t", col.names = T, row.names = F)
