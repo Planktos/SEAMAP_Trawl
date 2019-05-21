93417ac7edfc0360bb321ebbd1803d5e0f5f2377
Kelly Robinson <kellylynnrobinson@gmail.com>
2019-05-21 15:07:04 -0500

    revised file from KR_rev to aidyn_plots
    2019-05-21

--- process_SEAMAP_For_ArcMap.R
+++ process_SEAMAP_For_ArcMap.R
@@ -10,20 +10,31 @@ library(plyr)
 library(dplyr)
 library(data.table)
 
-d <- as.data.frame(fread(input = "Aurelia_SEAMAP.csv", stringsAsFactors = F, sep = ",", header = T))
+d <- as.data.frame(fread(input = "CHRYSAORA_SEAMAP.csv", stringsAsFactors = F, sep = ",", header = T))
 
-d$AggGrp_20130430 <- ifelse(test = is.na(d$AggGrp_20130430), "large jellyfish", d$AggGrp_20130430)
+d <- d[,c("AggGrp_20130430","Year", "Month", "Day","STATIONID","Subregion_Depth","Subregion_alongshore",
+          "Use_Pop_Den_no_m2","Use_Biomass_Den_kg_m2","Use_Depth_m", "DECSLAT","DECSLON","DECELAT","DECELON"),]
 
-d <- d[,c("AggGrp_20130430","Year", "Month", "Day","STATIONID", "Use_Pop_Den_no_m2","Use_Biomass_Den_kg_m2","Use_Depth_m", "DECSLAT","DECSLON","DECELAT","DECELON"),]
 names(d) <- tolower(names(d))
 d <- plyr::rename(d, c("use_depth_m" = "depth_m"))
 d <- plyr::rename(d, c("agggrp_20130430" = "agg_grp"))
 
-d$biomass_den_kg.m3 <- d$use_biomass_den_kg_m2*d$depth_m
-d$pop_den_no.m3 <- d$use_pop_den_no_m2*d$depth_m
+grp.name <- as.data.frame(unique(d$agg_grp))
+colnames(grp.name) <- c("name")
+grp.name <- as.character(grp.name[!is.na(grp.name$name),])
 
-d$biomass_den_kg.m3 <- ifelse(test = is.na(d$biomass_den_kg.m3), 0, d$biomass_den_kg.m3)
-d$pop_den_no.m3 <- ifelse(test = is.na(d$pop_den_no.m3), 0, d$pop_den_no.m)
+#assign agg_grp name to records with NA values if dealing with only one agg_group
+if(length(grp.name) == 1){
+
+  d$agg_grp <- with(d, ifelse(is.na(d$agg_grp), grp.name, d$agg_grp))
+
+  }
+
+d$biomass_den_kg_m3 <- d$use_biomass_den_kg_m2*d$depth_m
+d$pop_den_no_m3 <- d$use_pop_den_no_m2*d$depth_m
+
+d$biomass_den_kg_m3 <- ifelse(test = is.na(d$biomass_den_kg_m3), 0, d$biomass_den_kg_m3)
+d$pop_den_no_m3 <- ifelse(test = is.na(d$pop_den_no_m3), 0, d$pop_den_no_m3)
 
 f_mean_lat <- function(x){
 
@@ -56,9 +67,10 @@ f_mean_lon <- function(x){
 d$decslat_ctr <- apply(X = d, MARGIN = 1, FUN = f_mean_lat)
 d$decslon_ctr <- apply(X = d, MARGIN = 1, FUN = f_mean_lon)
 
-d$decslat <- NULL
-d$decelat <- NULL
-d$decslon <- NULL
-d$decelon <- NULL
+d[, c("decslat", "decelat", "decslon", "decelon")] = list(NULL)
 
-write.table(d, file = paste0(unique(d$agg_grp), "_arc.txt"), sep =  "\t", col.names = T, row.names = F)
+d <- d %>% select(stationid, agg_grp, year, month, day, depth_m, decslat_ctr, decslon_ctr, everything())
+
+d$taxa <- "CHRYSAORA"
+
+write.csv(x = d, file = paste0(unique(d$agg_grp), "_arc.csv"), row.names = F)
