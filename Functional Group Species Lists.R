6ae6447a2aff6460eb1ab4df87bf632c9802bf8b
Stacy <stacy.calhoun1@louisiana.edugit config --global user.email stacy.calhoun1@louisiana.edugit config --global user.email>
2020-06-09 13:12:04 -0500

    Adding new files; one being the updated NameTranslator table for oceanic model

---
+++ Functional Group Species Lists.R
@@ -0,0 +1,46 @@
+#Code to list species functional groups from Name Translator Table in order to pull biomass data from SEAMAP
+#Stacy Calhoun; created 5 Aug 2019
+
+
+remove(list=ls())
+assign("last.warning", NULL, envir = baseenv())
+
+
+
+# Libraries ---------------------------------------------------------------
+
+library(plyr)
+library(tidyverse)
+library(readxl)
+library(data.table)
+library(lubridate)
+
+
+# Data --------------------------------------------------------------------
+
+NameTrans = read_xlsx("NameTranslator_table201305.xlsx")
+
+NameTrans = subset(NameTrans, NameTrans$AggGrp_20130430 != 'ignore')
+
+
+
+# Subsetting by Functional Group ------------------------------------------
+FG = as.list(unique(NameTrans$AggGrp_20130430))
+FG = as.data.frame(FG)
+
+FG_list = split(NameTrans, f = NameTrans$AggGrp_20130430)
+
+
+
+FuncList = function(x)
+{
+  Sp = as.list((unique(x$TAXONOMIC)))
+  #names(Sp) = paste("FuncGrp", x, sep = "_")
+   return(Sp)
+}
+
+
+Species_List = lapply(FG_list, FuncList)
+
+
+
